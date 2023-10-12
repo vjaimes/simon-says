8f2680179f7ef1e58548f7ceef1c19b6e01a9d33
vjaimes <v_j38@txstate.edu>
2023-10-11 22:02:45 -0500

    added colorchanging function for squares. still working out the chinks

--- script.js
+++ script.js
@@ -6,19 +6,29 @@ const counter = document.querySelector(".counter");
 
 counter.textContent = counterStart;
 
-const reduceCounter = setInterval(function () {
-  counterStart--;
-  counter.textContent = counterStart;
+// game start count down
+const reduceCounter = function () {
+  setInterval(function () {
+    counterStart--;
+    counter.textContent = counterStart;
 
-  if (counterStart < 1) {
-    clearInterval(reduceCounter);
-    counter.style.display = "none";
-  }
-}, 1000);
+    if (counterStart < 1) {
+      clearInterval(reduceCounter);
+      counter.style.display = "none";
+    }
+  }, 1000);
+};
+
+const selectColor = function (target) {
+  setInterval(() => {
+    target.style.backgroundColor = "white";
+  }, 500);
+};
 
 document.addEventListener("click", function (e) {
   if (!e.target.classList.contains("square-size")) return;
   console.log(e.target.dataset.order);
+  selectColor(e.target);
 });
 
 // reduceCounter();
